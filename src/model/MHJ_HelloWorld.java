1b3c57c6761f07da41389cb09c5515c21dbc5f66
kot8585 <ansgywjd321@gmail.com>
2021-01-18 23:28:17 +0900

    브런치 생성해서 다시 함

--- src/model/MHJ_HelloWorld.java
+++ src/model/MHJ_HelloWorld.java
@@ -4,7 +4,7 @@ public class MHJ_HelloWorld {
 
 public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	System.out.println("Hello, 문효정!");
+	System.out.println("Hello, 문효정12!");
 }
 
 }
