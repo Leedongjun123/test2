c010079eb7b20e647f71482182ed3f7b5bd76a81
aorca603 <aorca603@gmail.com>
2021-01-18 22:32:37 +0900

    김평기가 커밋했습니다.

---
+++ src/model/KPK_HelloWorld.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * 
+ * @author ±Ë∆Ú±‚
+ * @version 1
+ */
+public class KPK_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, ±Ë∆Ú±‚!");
+	}
+
+}
